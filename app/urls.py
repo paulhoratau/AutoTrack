0b3f40c0286bf1cc26a31bfa561e931ffac21426
Paul <paulhoratau@gmail.com>
2024-11-21 18:32:25 +0200

    Added user registration and login functionality

--- app/urls.py
+++ app/urls.py
@@ -1,2 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', views.CreateUserView.as_view(), name='register')
+]
